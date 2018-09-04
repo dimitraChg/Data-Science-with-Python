d4f7c23f2badb2ec71124d0f1e6a9b383604f03b
dimitraChg <42813996+dimitraChg@users.noreply.github.com>
2018-09-05 00:31:34 +0300

    update working with pandas series and dataframes

--- Python Tutorial/Working with pandas Series and DataFrames.py
+++ Python Tutorial/Working with pandas Series and DataFrames.py
@@ -1,9 +1,6 @@
 import numpy as np
 import pandas as pd
-
 from pandas import Series, DataFrame
-
-## Create a Series object with values of 0-7
 
 series_obj = Series(np.arange(8), index=['row 1', 'row 2', 'row 3', 'row 4', 'row 5', 'row 6', 'row 7', 'row 8'])
 print(series_obj)
@@ -12,11 +9,7 @@ print(series_obj['row 7'])
 
 print(series_obj[[0, 7]])
 
-## Slice a Series object
-
 print(series_obj['row 3': 'row7])
-
-## Create a DataFrame object with random values from 0-1 in each row and column
 
 np.random.seed(25)
 DF_obj = DataFrame(np.random.rand(36).reshape((6,6)), 
@@ -26,3 +19,9 @@ print(DF_obj)
 
 print(DF_obj.ix[['row 2', 'row 5'], ['column 5', 'column 2']])
 
+print(DF_obj < 0.2)
+
+print(series_obj[series_obj > 6])
+
+series_obj['row 1', 'row 5', 'row 8'] = 8
+print(series_obj)
